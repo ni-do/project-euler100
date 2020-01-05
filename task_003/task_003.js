e4c1b4013480cca9aa987dcd9431940e7ee4fb5c
Niklas Donath <niklas.donath@web.de>
2020-01-06 00:22:33 +0100

    task 3 finished

--- task_003/task_003.js
+++ task_003/task_003.js
@@ -3,7 +3,6 @@
 
 const isPrimeNumber = (number) => {
     if (number < 2) return false
-
     let i = 2
     while (i < number) {
         if(number % i === 0) return false
@@ -12,22 +11,13 @@ const isPrimeNumber = (number) => {
     return true
 }
 
-const isDividableByTwo = (number) => {
-    return (number % 2 === 0)
-}
-
-const isDividableByFive = (number) => {
-    return (number % 5 === 0)
-}
-
-const isDividableByThree = (number) => {
-    return (number % 3 === 0)
+const isDividableBy = (divident, divisor) => {
+    return (divident % divisor === 0)
 }
 
 const getQuerSumFrom = (number) => {
     let digits = number.toString().split('')
-    return digits.reduce((acc, currItem) => acc + currItem, 0)
-    
+    return digits.reduce((acc, currItem) => acc + +currItem, 0)
 }
 
 const getNextPrimeNumberTo = (primeNumber) => {
@@ -37,16 +27,29 @@ const getNextPrimeNumberTo = (primeNumber) => {
 }
 
 const getLargestPrimeFactorOf = (number) => {
-    const primeFactors = [1]
-    while(primeFactors.reduce((acc, currItem) => acc * currItem, 1) <= number) {
-        primeFactors.push(getNextPrimeNumberTo(primeFactors[primeFactors.length - 1]))
-    }
-    console.log('primeFactors:', primeFactors)
+    const primeFactors = []
+    let currentPrimeNumber = 2
+
+    let rest = number
+    do {
+        if(currentPrimeNumber !== 3 && isDividableBy(rest, currentPrimeNumber)) {
+            rest /= currentPrimeNumber
+            primeFactors.push(currentPrimeNumber)
+        } else if(isDividableBy(getQuerSumFrom(rest), 3)) {
+            rest /= 3
+            primeFactors.push(3)
+        } else {
+            currentPrimeNumber = getNextPrimeNumberTo(currentPrimeNumber)
+        }
+    } while (!isPrimeNumber(rest))
+    primeFactors.push(rest)
     return primeFactors.pop()
 }
 
-const result1 = getLargestPrimeFactorOf(13195)
-const result2 = getLargestPrimeFactorOf(600851475143)
+const result1 = getLargestPrimeFactorOf(616)
+const result2 = getLargestPrimeFactorOf(13195)
+const result3 = getLargestPrimeFactorOf(600851475143)
 
 console.log('result1:', result1)
 console.log('result2:', result2)
+console.log('result3:', result3)
