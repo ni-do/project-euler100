bbdb4aa010ea1bb653243ec57d8362b49819f495
Niklas Donath <niklas.donath@web.de>
2020-01-11 23:43:15 +0100

    solved task #9

--- task_009/task_009.js
+++ task_009/task_009.js
@@ -7,10 +7,18 @@
 // There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 // Find the product abc.
 
-let a = 3, b = 4
+const getPythagoreanTriplet = () => {
+    let a = 0, b = 0, c = 0
+    for(a = 1; a < 1000; a++) {
+        for(b = a; b < 1000; b++) {
+            let c = 1000 - a - b
+            if (a**2 + b**2 === c**2) {
+                return { a, b, c };
+            }
+        }
+    }
+    throw Error('could not find pythagorean triplet for a + b + c = 1000')
+}
 
-do {
-    b % 2 === 0 ? b++ : a++
-} while (a**2 + b**2 < 1000) 
-
-console.log('result: a:', a, 'b:', b, 'a^2 + b^2=', a**2 + b**2)
+const { a, b, c } = getPythagoreanTriplet()
+console.log('result: a:', a, 'b:', b, 'c:', c)
