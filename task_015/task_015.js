0385984aa20e3a4836e648875d922e22d31f28ff
Niklas Donath <niklas.donath@web.de>
2020-02-03 00:09:12 +0100

    WIP: created generator for grid

---
+++ task_015/task_015.js
@@ -0,0 +1,20 @@
+// Starting in the top left corner of a 2×2 grid, 
+// and only being able to move to the right and down, 
+// there are exactly 6 routes to the bottom right corner.
+// How many such routes are there through a 20×20 grid?
+
+
+const createGrid = (x, y) => {
+    return Array(y).fill(Array(x).fill("x"))
+}
+
+const moveRight = (grid) => {
+
+}
+
+const moveDown = (grid) => {
+    
+}
+
+const grid = createGrid(20,20)
+
