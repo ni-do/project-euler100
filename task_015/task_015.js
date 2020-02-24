eb9dab546bb331c0d33be004ea73625da625b7be
Niklas Donath <niklas.donath@web.de>
2020-02-24 23:41:55 +0100

    solved using combinatorics

--- task_015/task_015.js
+++ task_015/task_015.js
@@ -3,18 +3,14 @@
 // there are exactly 6 routes to the bottom right corner.
 // How many such routes are there through a 20×20 grid?
 
-
-const createGrid = (x, y) => {
-    return Array(y).fill(Array(x).fill("x"))
+const getNumofPathsForGridSize = (gridSize) => {
+    let paths = 1
+    for (let i = 0; i < gridSize; i++) {
+        paths *= (2 * gridSize) - i;
+        paths /= i + 1;
+    }
+    return paths
 }
 
-const moveRight = (grid) => {
-
-}
-
-const moveDown = (grid) => {
-    
-}
-
-const grid = createGrid(20,20)
-
+console.log('paths for grid size 2x2:', getNumofPathsForGridSize(2))
+console.log('paths for grid size 20x20:', getNumofPathsForGridSize(20))
